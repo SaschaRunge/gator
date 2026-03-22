6f46fe668bbf9700f2629663f7ecef7c98fee99a
SaschaRunge <sascharunge89@yahoo.de>
2026-03-22 18:19:47 +0100

    setup postgres sql support

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 
 	"github.com/SaschaRunge/gator/internal/config"
+	"github.com/SaschaRunge/gator/internal/database"
 )
 
 type state struct {
-	config *config.Config
+	config    *config.Config
+	dbQueries *database.Queries
+}
+
+func NewState(cfg *config.Config, dbQueries *database.Queries) state {
+	return state{cfg, dbQueries}
 }
 
 type command struct {
@@ -20,30 +26,54 @@ type commands struct {
 	commands map[string]func(*state, command) error
 }
 
-func (c *commands) run(s *state, cmd command) error {
-	callback, exists := c.commands[cmd.name]
-	if !exists {
-		errMsg := fmt.Sprintf("%s is not a valid command.", cmd.name)
-		return errors.New(errMsg)
+func NewCommand(args []string) (command, error) {
+	if len(args) == 0 {
+		return command{}, errors.New("No arguments supplied for creation of command.\n")
 	}
 
-	return callback(s, cmd)
+	if len(args) == 1 {
+		return command{
+			name: args[0],
+			args: []string{},
+		}, nil
+	} else {
+		return command{
+			name: args[0],
+			args: args[1:],
+		}, nil
+	}
+}
+
+func NewCommands() commands {
+	cmds := commands{make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+	return cmds
 }
 
 func (c *commands) register(name string, f func(*state, command) error) {
 	c.commands[name] = f
 }
 
+func (c *commands) Run(s *state, cmd command) error {
+	callback, exists := c.commands[cmd.name]
+	if !exists {
+		errMsg := fmt.Sprintf("%s is not a valid command.\n", cmd.name)
+		return errors.New(errMsg)
+	}
+
+	return callback(s, cmd)
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
-		errMsg := fmt.Sprintf("Missing argument for command. Usage: %s <username>", cmd.name)
+		errMsg := fmt.Sprintf("Missing argument for command. Usage: %s <username>\n", cmd.name)
 		return errors.New(errMsg)
 	}
 
 	if err := s.config.SetUser(cmd.args[0]); err != nil {
 		return err
 	} else {
-		fmt.Printf("Current user: %s", cmd.args[0])
+		fmt.Printf("Current user: %s\n", cmd.args[0])
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
-import (
-	"fmt"
+import _ "github.com/lib/pq"
 
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/SaschaRunge/gator/internal/cli"
 	"github.com/SaschaRunge/gator/internal/config"
+	"github.com/SaschaRunge/gator/internal/database"
 )
 
 func main() {
 	cfg, _ := config.Read()
-	cfg.SetUser("Sascha")
-	cfg, _ = config.Read()
+
+	db, err := sql.Open("postgres", cfg.Db_url)
+	dbQueries := database.New(db)
+
+	state := cli.NewState(&cfg, dbQueries)
+	cmds := cli.NewCommands()
+
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Missing command name in input. Exiting.\n")
+		os.Exit(1)
+	}
+
+	cmd, _ := cli.NewCommand(args[1:])
+	err = cmds.Run(&state, cmd)
+	if err != nil {
+		fmt.Printf("Could not run command: '%s'\n", err)
+		os.Exit(1)
+	}
 
 	fmt.Printf("%+v", cfg)
 }
